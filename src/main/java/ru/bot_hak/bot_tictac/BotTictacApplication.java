cce02694f2122b12ef837f7495bdddbae3f1e7d1
AXBIT\emarchenko <emarchenko@axbit.ru>
2023-11-13 21:44:07 +0400

    Init

---
+++ src/main/java/ru/bot_hak/bot_tictac/BotTictacApplication.java
@@ -0,0 +1,13 @@
+package ru.bot_hak.bot_tictac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotTictacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotTictacApplication.class, args);
+	}
+
+}
